39764f178d8340490d6e20d1899f1600bdcd83a9
Casey Kusella-Gussin <ckgussindev@googlemail.com>
2023-04-27 01:08:24 +0100

    Level added and Lighting

---
+++ Website_Portfolio/Assets/Scripts/First_Person_Contoller.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class First_Person_Contoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public CharacterController Controller;
+    public float speed = 12f;
+    private Vector3 velocity;
+    public float gravity = -9.81f;
+    
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = (transform.right * x + transform.forward * z);
+
+        Controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity*Time.deltaTime;
+
+        Controller.Move(velocity * Time.deltaTime);
+
+    }
+}
