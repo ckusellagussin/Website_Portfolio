4836c693f1581fa0042de764399bcecad4dc0089
Casey Kusella-Gussin <ckgussindev@googlemail.com>
2023-05-31 23:16:37 +0100

    Button Update
    Buttons are now spread across the level and will now change material when a mouse hovers over them
    Certain buttons will show user to links while others will show more information

---
+++ Website_Portfolio/Assets/Scripts/Link_Manager.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Searcher;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Link_Manager : MonoBehaviour
+{
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private string selectableTag2 = "Github";
+    [SerializeField] private string selectableTag3 = "Twitter";
+    [SerializeField] private string selectableTag4 = "LinkedIn";
+    [SerializeField] private string selectableTag5 = "Youtube";
+    [SerializeField] private string selectableTag6 = "GodsOS";
+    [SerializeField] private string selectableTag7 = "InstaFitness";
+    [SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        
+        if (Physics.Raycast(ray, out hit, 50))
+        { 
+            var selection = hit.transform;
+            
+            if (selection.CompareTag(selectableTag2))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                    selectionRenderer.material = highlightMaterial;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                   Application.OpenURL("https://github.com/ckusellagussin/");
+                   
+                }
+                
+
+                _selection = selection;
+            }
+            
+            
+            if (selection.CompareTag(selectableTag3))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                    selectionRenderer.material = highlightMaterial;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    Application.OpenURL("https://twitter.com/CKGussinDev/");
+
+                }
+                
+
+                _selection = selection;
+            }
+            
+            if (selection.CompareTag(selectableTag4))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                    selectionRenderer.material = highlightMaterial;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    Application.OpenURL("https://www.linkedin.com/in/casey-kusella-gussin-a911b675/");
+
+                }
+                
+
+                _selection = selection;
+            }
+            
+            if (selection.CompareTag(selectableTag5))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                    selectionRenderer.material = highlightMaterial;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    Application.OpenURL("https://www.youtube.com/channel/UCx_gTGVxOKB_a08csWiwvGg");
+
+                }
+                
+
+                _selection = selection;
+            }
+            
+            if (selection.CompareTag(selectableTag6))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                    selectionRenderer.material = highlightMaterial;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    Application.OpenURL("https://steamcommunity.com/sharedfiles/filedetails/?id=2497121194");
+
+                }
+                
+
+                _selection = selection;
+            }
+            
+            if (selection.CompareTag(selectableTag7))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                    selectionRenderer.material = highlightMaterial;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    Application.OpenURL("https://www.instagram.com/ckg_fitness/");
+
+                }
+                
+
+                _selection = selection;
+            }
+            
+            
+        }
+        
+        
+
+    }
+}
---
+++ Website_Portfolio/Assets/Scripts/Selection_Manager.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Searcher;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Selection_Manager : MonoBehaviour
+{
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private string selectableTag8 = "GameJam";
+    [SerializeField] private GameObject popupUI;
+    [SerializeField] private GameObject popupUI2;
+    private Transform _selection;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        
+        if (Physics.Raycast(ray, out hit, 50))
+        { 
+            var selection = hit.transform;
+            
+            if (selection.CompareTag(selectableTag))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                    selectionRenderer.material = highlightMaterial;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    if (popupUI.activeSelf != true)
+                    {
+        
+                        popupUI.SetActive(true);
+                        Debug.Log("You have hit the button with mouse and made the UI pop");
+
+                    }
+                    
+                    else
+                    {
+                        
+                         Debug.Log("You have hit the button and UI has hidden again");
+                         popupUI.SetActive(false);
+                         
+                         
+                    }
+                    
+
+                }
+                
+
+                _selection = selection;
+            }
+            
+            
+            if (selection.CompareTag(selectableTag8))
+            {
+                
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                    selectionRenderer.material = highlightMaterial;
+
+                }
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+
+                    if (popupUI2.activeSelf != true)
+                    {
+        
+                        popupUI2.SetActive(true);
+                        Debug.Log("You have hit the button with mouse and made the UI pop");
+
+                    }
+                    
+                    else
+                    {
+                        
+                        popupUI2.SetActive(false);
+                        Debug.Log("You have hit the button and UI has hidden again");
+
+                    }
+                    
+
+                }
+                
+
+                _selection = selection;
+            }
+            
+            
+        }
+        
+        
+
+    }
+}
